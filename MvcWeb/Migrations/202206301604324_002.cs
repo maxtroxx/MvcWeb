a1d3c2a044dadf18630932cc7e3ed9cecdb0dbe7
Nicolas Barra <nikoxz40@hotmail.com>
2022-06-30 13:19:41 -0400

    nueva imagenes

--- MvcWeb/Migrations/202206301604324_002.cs
+++ MvcWeb/Migrations/202206301604324_002.cs
@@ -7,9 +7,21 @@
     {
         public override void Up()
         {
-            AddColumn("dbo.Productoes", "imagen", c => c.String());
+            CreateTable(
+                "dbo.Productoes",
+                c => new
+                {
+                    Id = c.Int(nullable: false, identity: true),
+                    nombre = c.String(nullable: false),
+                    stock = c.Int(nullable: false),
+                    precio = c.Int(nullable: false),
+                    descripcion = c.String(),
+                    imagen = c.String()
+                })
+                .PrimaryKey(t => t.Id);
+
         }
-        
+
         public override void Down()
         {
             DropColumn("dbo.Productoes", "imagen");
