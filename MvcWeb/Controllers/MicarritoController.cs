636c9ee1db04bb9d393f3992ad87f62ec8e7140a
maxy <luis.valenzuela.tablet@gmail.com>
2022-06-30 00:58:17 -0400

    Micarrito y btns produc

--- MvcWeb/Controllers/MicarritoController.cs
+++ MvcWeb/Controllers/MicarritoController.cs
@@ -6,12 +6,84 @@ using System.Web.Mvc;
 
 namespace MvcWeb.Controllers
 {
-    public class MicarritoController : Controller
+    public class MiCarritoController : Controller
     {
-        // GET: Micarrito
+        // GET: MiCarrito
         public ActionResult Index()
         {
             return View();
+        }
+
+        // GET: MiCarrito/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: MiCarrito/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: MiCarrito/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: MiCarrito/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: MiCarrito/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: MiCarrito/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: MiCarrito/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
         }
     }
 }
