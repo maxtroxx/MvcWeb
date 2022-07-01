7f3183d64d4e7745fe173127327c251af62233a3
maxy <luis.valenzuela.tablet@gmail.com>
2022-07-01 14:45:33 -0400

    Final

---
+++ MvcWeb/Models/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MvcWeb.Models
+{
+    public class Class1
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string nombreComprador { get; set; }
+        public int idComprador { get; set; }
+
+    }
+}
